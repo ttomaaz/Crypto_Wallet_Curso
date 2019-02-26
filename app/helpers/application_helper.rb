f53e24e9818e23b2807f210c422130ec78ea5cdc
ttomaaz <tomazmartinscrowd@gmail.com>
2019-02-26 16:56:00 -0300

    Helper para mostrar em qual ambiente do Rails estamos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def convtime (us_date)
         us_date.strftime("%d/%m/%Y")
   end
+
+  def ambiente_desenvolvimento
+        if Rails.env.development?
+              "Desenvolvimento"
+          elsif Rails.env.production?
+              "Produção"
+          else Rails.env.test?
+              "Teste"
+        end
+
+  end
 end
