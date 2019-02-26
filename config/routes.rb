b25d3448fc3ed1daffea2450fe30a6b494bbe248
ttomaaz <tomazmartinscrowd@gmail.com>
2019-02-26 15:41:33 -0300

    entendendo um pouco mais sobre rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get '/inicio', to: 'welcome#index'
+
   resources :coins
+  #resources o rails usa para criar algumas rotas padrão para a sua aplicação
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   ##Usando a instrução root to: para indicar as rotas
