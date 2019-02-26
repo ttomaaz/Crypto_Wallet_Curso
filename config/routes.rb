b26db6f70b712d5e7df5d848400ef1f4c4cfa4eb
ttomaaz <tomazmartinscrowd@gmail.com>
2019-02-26 15:26:31 -0300

    iniciando as rotas no routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :coins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  ##Usando a instrução root to: para indicar as rotas
+
+  root to: "welcome#index"
 end
