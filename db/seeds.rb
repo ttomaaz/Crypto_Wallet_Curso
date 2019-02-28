ef2d51b998c6d773a8ee13434c5aa7dacdd23301
ttomaaz <tomazmartinscrowd@gmail.com>
2019-02-28 13:56:50 -0300

    Mexendo no arquivo seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/600px-ETHEREUM-YOUTUBE-PROFILE-PIC.png"
+)
+
+Coin.create!(
+  description: "Litecoin",
+  acronym: "LTC",
+  url_image: "https://banner2.kisspng.com/20180320/kqq/kisspng-brand-trademark-logo-litecoin-5ab088952b3b84.0667612615215187411771.jpg"
+)
+
+puts "Moedas cadastradas com sucesso!"
