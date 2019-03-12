335650475a49e1cddc58978fcbcd6d5846485ff9
ttomaaz <tomazmartinscrowd@gmail.com>
2019-03-12 16:14:28 -0300

    Entendendo um pouco sobre YAML

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'pry-rails'
 # Usado para criar um spinner na hora do loading de algum processo
 gem 'tty-spinner'
+# Usado para internacionalização
+gem 'rails-i18n', '~> 5.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,19 @@
 module ApplicationHelper
+  def locale
+    I18n.locale == :en ? "Estados Unidos" : "Brasil"
+  end
+
   def convtime (us_date)
-        us_date.strftime("%d/%m/%Y")
+    us_date.strftime("%d/%m/%Y")
   end
 
   def ambiente_desenvolvimento
-        if Rails.env.development?
-              "Desenvolvimento"
-          elsif Rails.env.production?
-              "Produção"
-          else Rails.env.test?
-              "Teste"
-        end
-
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else Rails.env.test?
+      "Teste"
+    end
   end
-
-  
 end
